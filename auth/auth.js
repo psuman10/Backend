564f1c9e9c6f9a1d07ffa577d1d711fe1411d60a
psuman10 <psuman9840@gmail.com>
2021-12-19 10:32:41 +0545

    created auth and user routes

---
+++ auth/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const user = require("../models/userModel");
+
+
+
+module.exports.verifyUser = function(req,res,next) {
+    try{
+        token = req.headers.authorization.split(" ")[1];
+       const data= jwt.verify(token,"aysecretkey");
+        user.findOne({_id : data.cid}).then(function(result){
+            req.userInfo = result;
+            next();
+        })
+        .catch(function(error){
+            res.json({msg:"Invalid user!!"})
+            
+        })
+    }
+    catch(error){
+        res.json({msg:"Invalid Token!!"});
+    }
+    
+}
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/userModel");
 const bcryptjs= require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const auth= require('../auth/auth');
 
 
 router.post("/login", async(req, res) => {
@@ -65,6 +66,50 @@ router.post("/register", async(req, res) => {
     
 
 });
+
+router.put("/user/profile/update",auth.verifyUser, async(req, res) => {
+    //console.log(req.customerInfo._id)
+    const id = req.userInfo._id;
+    const username = req.body.username;
+    user.updateOne({_id : id},{username : username}).then(function() {
+        res.json({msg: "Update Success!"})
+
+
+    }).catch(function(e){
+        res.json({msg:"Something went wrong!"})
+
+    })
     
-module.exports = router
+
+})
+
+
+//customer delete by themselves
+
+router.delete("/user/delete",auth.verifyUser, async(req, res) => {
+    const id = req.userInfo._id;
+    User.findByIdAndDelete(id).then(function() {
+        res.json({msg: "Deleted Successfully!"})
+
+    }).catch(function(e){
+        res.json({msg:"Something went wrong!"})
+    })
+})
+
+
+//customer delete by different user
+// Admin is required
+
+// router.delete("/customer/delete/admin",auth.verifyAdmin, async(req, res) => {
+//     const id = req.adminInfo._id;
+//     const cid =  req.body.cid   // customer id which is to be deleted
+//     customer.deleteOne({_id : cid}).then(function() {
+//         res.json({msg: "Deleted Successfully!"})
+
+//     }).catch(function(e){
+//         res.json({msg:"Something went wrong!"})
+//     })
+// })
+
+module.exports = router;
 
