31b0527abea37a73800a717048b37cde1616ce10
psuman10 <psuman9840@gmail.com>
2021-12-23 20:14:48 +0545

    small changes on user model code

--- models/userModel.js
+++ models/userModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
      username : {type:String , required: true, unique: true},
      email : {type:String, required : true},
+     Number: {type:String, required: true},
      password : {type:String , required: true}
 
 })
