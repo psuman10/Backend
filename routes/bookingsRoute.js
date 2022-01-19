e76a365bb82837b0e15485d3595df870e95816f1
psuman10 <psuman9840@gmail.com>
2022-01-19 18:40:26 +0545

    modified booking routes

---
+++ routes/bookingsRoute.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const Booking = require("../models/bookingModel");
+
+router.post("/bookcar", async (req, res) => {
+  const { token } = req.body;
+  try {
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
+
+
+router.get("/getallbookings", async(req, res) => {
+
+    try {
+
+        const bookings = await Booking.find().populate('car')
+        res.send(bookings)
+        
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+  
+});
+
+
+
+
+module.exports = router;
